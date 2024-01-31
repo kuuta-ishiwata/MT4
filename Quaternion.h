32c438e7956f584692d9847b16cf388ac3a06ffa
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2024-01-31 21:12:43 +0900

    mt

---
+++ Quaternion.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+struct Quaternion {
+	float x;
+	float y;
+	float z;
+	float w;
+};
+
+// 積
+Quaternion Multiply(const Quaternion& lhs, const Quaternion& rhs);
+
+
+// 単位Quaternion
+Quaternion IdentituQuaternion();
+
+
+// 共役Quaternionを返す
+Quaternion Conjugate(const Quaternion& quaternion);
+
+// 正規化したQuaternionを返す
+Quaternion Normalize(const Quaternion& quaternion);
+
+
+// 逆Quaternionを返す
+Quaternion Inverse(const Quaternion& quaternion);
+
+// Quaternionのnormを返す
+float Norm(const Quaternion& quaternion);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <Novice.h>
 #include "MATH.h"
-
+#include "Quaternion.h"
+#include "ImGuiManager.h"
 const char kWindowTitle[] = "LE2C_02_イシワタ_クウタ_MT4";
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -13,15 +14,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	Vector3 from0 = Normalize(Vector3{1.0f, 0.7f, 0.5f});
-	Vector3 to0 = Multiply(from0, -1);
-	Vector3 from1 = Normalize(Vector3{-0.6f, 0.9f, 0.2f});
-	Vector3 to1 = Normalize(Vector3{0.4f, 0.7f, -0.5f});
-
-	Matrix4x4 rotateMatrix0 = DirectionToDirection(
-	    Normalize(Vector3{1.0f, 0.0f, 0.0f}), Normalize(Vector3{-1.0f, 0.0f, 0.0f}));
-	Matrix4x4 rotateMatrix1 = DirectionToDirection(from0, to0);
-	Matrix4x4 rotateMatrix2 = DirectionToDirection(from1, to1);
+	Quaternion p1 = {2.0f, 3.0f, 4.0f, 1.0f};
+	Quaternion p2 = {1.0f, 3.0f, 5.0f, 2.0f};
+	Quaternion identity = IdentituQuaternion();
+	Quaternion conj = Conjugate(p1);
+	Quaternion inv = Inverse(p1);
+	Quaternion normal = Normalize(p1);
+	Quaternion mul1 = Multiply(p1, p2);
+	Quaternion mul2 = Multiply(p2, p1);
+	float norm = Norm(p1);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -44,10 +45,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		
-		MatrixScreenPrintf(0, 20, rotateMatrix0, "rotateMatrix0");
-		MatrixScreenPrintf(0, 20 + 20 * 5, rotateMatrix1, "rotateMatrix1");
-		MatrixScreenPrintf(0, 20 + 20 * 10, rotateMatrix2, "rotateMatrix2");
-	
+		//ImGuiで描画
+	    ImGui::Begin("Quaternion");
+		ImGui::Text( "Identity: %.2f  %.2f  %.2f  %.2f", identity.x, identity.y, identity.z,identity.w);
+		ImGui::Text("Conjugate: %.2f  %.2f  %.2f  %.2f", conj.x, conj.y, conj.z, conj.w);
+		ImGui::Text("Inverse: %.2f  %.2f  %.2f  %.2f", inv.x, inv.y, inv.z, inv.w);
+		ImGui::Text("Normalize: %.2f  %.2f  %.2f  %.2f", normal.x, normal.y, normal.z, normal.w);
+		ImGui::Text("Multiply(q1, q2) : %.2f  %.2f  %.2f  %.2f", mul1.x, mul1.y, mul1.z, mul1.w);
+		ImGui::Text("Multiply(q2, q1) : %.2f  %.2f  %.2f  %.2f", mul2.x, mul2.y, mul2.z, mul2.w);
+		ImGui::Text("Norm:%.2f", norm);
+		ImGui::End();
 
 		/// ↑描画処理ここまで
 		///
